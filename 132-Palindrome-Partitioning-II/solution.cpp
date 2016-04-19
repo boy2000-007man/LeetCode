c349c86a4229fd27a96c214f6a70ce2ea780b780
Guo xuenan <boy2000_007man@163.com>
2016-04-19 13:42:29 +0800

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.cpp
+++ 132-Palindrome-Partitioning-II/solution.cpp
@@ -2,34 +2,23 @@ class Solution {
     int search(string &s, vector<int> &count, int len) {
         if (count[len] != INT_MAX)
             return count[len];
-        vector<int> equal(len + 1 >> 1, 1),
-                    offset(equal.size(), 1);
-        for (int i = 1; i < equal.size(); i++) {
-            equal[i] = equal[i - 1];
-            while (0 <= i - equal[i]
-                && s[len - 1 - i] != s[len - 1 - (i - equal[i])])
-                equal[i] += offset[i - equal[i]];
-            offset[i] = equal[i - 1];
-            while (0 <= i - offset[i]
-                && s[len - 1 - i] == s[len - 1 - (i - offset[i])])
-                offset[i] += offset[i - offset[i]];
+        int size = len + 1 >> 1;
+        int equal[size] = {1},
+            offset[size] = {1};
+        for (int i = 1; i < size; i++) {
+            for (equal[i] = equal[i - 1]; 0 <= i - equal[i] && s[len - 1 - i] != s[len - 1 - (i - equal[i])]; equal[i] += offset[i - equal[i]]);
+            for (offset[i] = equal[i - 1]; 0 <= i - offset[i] && s[len - 1 - i] == s[len - 1 - (i - offset[i])]; offset[i] += offset[i - offset[i]]);
         }
         for (int i = 0, j = 0; i < len && count[len]; )
-            if (s[i] == s[len - 1 - j]) {
+            if (s[i] == s[len - 1 - j])
                 if (len - 1 - j <= i + 2) {
                     count[len] = min(count[len], search(s, count, i - j) + 1);
-                    if ((j -= equal[j]) < 0) {
-                        i++;
-                        j = 0;
-                    }
-                } else {
-                    i++;
-                    j++;
-                }
-            } else if ((j -= offset[j]) < 0) {
-                i++;
-                j = 0;
-            }
+                    if ((j -= equal[j]) < 0)
+                        i++, j = 0;
+                } else
+                    i++, j++;
+            else if ((j -= offset[j]) < 0)
+                i++, j = 0;
         return count[len];
     }
 public:
