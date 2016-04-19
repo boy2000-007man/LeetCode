bc824d58ae85d101c1f31e82300922879e578861
Guo xuenan <boy2000_007man@163.com>
2016-04-19 20:31:26 +0800

    [30. Substring with Concatenation of All Words][Accepted]committed by LeetCode Extension

--- 30-Substring-with-Concatenation-of-All-Words/solution.cpp
+++ 30-Substring-with-Concatenation-of-All-Words/solution.cpp
@@ -1,3 +1,44 @@
+struct dictionary_tree {
+    int num;
+    vector<pair<char, dictionary_tree *>> next;
+    dictionary_tree() : num(0), next(0) {
+    }
+    ~dictionary_tree() {
+        for (auto &p: next)
+            if (p.second)
+                delete p.second;
+    }
+    int &operator[](const char *s) {
+        dictionary_tree *r = this;
+        for (int i = 0; s[i]; i++) {
+            bool find = false;
+            for (int j = 0; j < r->next.size() && !find; j++)
+                if (s[i] == r->next[j].first) {
+                    find = true;
+                    r = r->next[j].second;
+                }
+            if (!find) {
+                r->next.push_back(make_pair(s[i], new dictionary_tree()));
+                r = r->next.back().second;
+            }
+        }
+        return r->num;
+    }
+    int *find(const char *s, int length) {
+        dictionary_tree *r = this;
+        for (int i = 0; i < length; i++) {
+            bool find = false;
+            for (int j = 0; j < r->next.size() && !find; j++)
+                if (s[i] == r->next[j].first) {
+                    find = true;
+                    r = r->next[j].second;
+                }
+            if (!find)
+                return NULL;
+        }
+        return &r->num;
+    }
+};
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
@@ -5,10 +46,10 @@ public:
                     count(1, 0),
                     bitmap(256, 0),
                     bitmap_own(256, 0);
-        unordered_map<string, int> s2i;
+        dictionary_tree s2i;
         int required = 0;
         for (string &word: words) {
-            int &i = s2i[word];
+            int &i = s2i[word.c_str()];
             if (!i) {
                 i = count.size();
                 count.push_back(1);
@@ -30,8 +71,8 @@ public:
                 bool sub = true;
                 vector<int> count_own(count.size(), 0);
                 for (int j = 0; j < words.size() && sub; j++) {
-                    auto it = s2i.find(s.substr(i - total_length + 1 + j * length, length));
-                    if (it == s2i.end() || count[it->second] < ++count_own[it->second])
+                    int *res = s2i.find(s.c_str() + i - total_length + 1 + j * length, length);
+                    if (!res || count[*res] < ++count_own[*res])
                         sub = false;
                 }
                 if (sub)
