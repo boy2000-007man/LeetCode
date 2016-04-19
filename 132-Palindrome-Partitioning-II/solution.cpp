5e72de8209035950baffdec40a4270e7310ebd55
Guo xuenan <boy2000_007man@163.com>
2016-04-19 17:51:33 +0800

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.cpp
+++ 132-Palindrome-Partitioning-II/solution.cpp
@@ -9,7 +9,7 @@ class Solution {
             for (equal[i] = equal[i - 1]; 0 <= i - equal[i] && s[len - 1 - i] != s[len - 1 - (i - equal[i])]; equal[i] += offset[i - equal[i]]);
             for (offset[i] = equal[i - 1]; 0 <= i - offset[i] && s[len - 1 - i] == s[len - 1 - (i - offset[i])]; offset[i] += offset[i - offset[i]]);
         }
-        for (int i = 0, j = 0; i < len && count[len] && depth < count.back(); )
+        for (int i = 0, j = 0; i < len && (bool)(i - j) < count[len] && depth + (bool)(i - j) < count.back(); )
             if (s[i] == s[len - 1 - j])
                 if (len - 1 - j <= i + 2) {
                     if (search(s, count, i - j, depth + 1) != INT_MAX)
