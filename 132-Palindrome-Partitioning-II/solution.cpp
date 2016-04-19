9d5db9ccb237ef035d945d4cd14eca4a90dbf8c8
Guo xuenan <boy2000_007man@163.com>
2016-04-19 17:44:37 +0800

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.cpp
+++ 132-Palindrome-Partitioning-II/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
-    int search(string &s, vector<int> &count, int len) {
+    int search(string &s, vector<int> &count, int len, int depth) {
         if (count[len] != INT_MAX)
             return count[len];
-        int size = len + 1 >> 1;
-        int equal[size] = {1},
+        int size = len + 1 >> 1,
+            equal[size] = {1},
             offset[size] = {1};
         for (int i = 1; i < size; i++) {
             for (equal[i] = equal[i - 1]; 0 <= i - equal[i] && s[len - 1 - i] != s[len - 1 - (i - equal[i])]; equal[i] += offset[i - equal[i]]);
             for (offset[i] = equal[i - 1]; 0 <= i - offset[i] && s[len - 1 - i] == s[len - 1 - (i - offset[i])]; offset[i] += offset[i - offset[i]]);
         }
-        for (int i = 0, j = 0; i < len && count[len]; )
+        for (int i = 0, j = 0; i < len && count[len] && depth < count.back(); )
             if (s[i] == s[len - 1 - j])
                 if (len - 1 - j <= i + 2) {
-                    count[len] = min(count[len], search(s, count, i - j) + 1);
+                    if (search(s, count, i - j, depth + 1) != INT_MAX)
+                        count[len] = min(count[len], count[i - j] + 1);
                     if ((j -= equal[j]) < 0)
                         i++, j = 0;
                 } else
@@ -25,6 +26,6 @@ public:
     int minCut(string s) {
         vector<int> count(s.length() + 1, INT_MAX);
         count[0] = -1;
-        return search(s, count, s.length());
+        return search(s, count, s.length(), 0);
     }
 };
