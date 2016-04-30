72cb23ed65cd646be852fb1693203af333e3cfc3
Xuenan Guo <boy2000_007man@163.com>
2016-05-01 01:47:35 +0800

    [338. Counting Bits][Accepted]committed by LeetCode Extension

--- 338-Counting-Bits/solution.cpp
+++ 338-Counting-Bits/solution.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        
+        vector<int> result(num + 1, 0);
+        for (int i = 1, j = 1; i < result.size(); i++) {
+            if (j << 1 <= i)
+                j <<= 1;
+            result[i] = result[i ^ j] + 1;
+        }
+        return result;
     }
 };
