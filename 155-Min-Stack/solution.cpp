aa591042cfbeed4730dc8f8bb6b5a2832eb9f5bb
Xuenan Guo <boy2000_007man@163.com>
2016-05-04 19:34:33 +0800

    [155. Min Stack][Accepted]committed by LeetCode Extension

--- 155-Min-Stack/solution.cpp
+++ 155-Min-Stack/solution.cpp
@@ -1,26 +1,31 @@
 class MinStack {
+    vector<int> stk;
+    vector<pair<int, int>> min_ele;
 public:
     /** initialize your data structure here. */
-    MinStack() {
-        
+    MinStack() : stk(0), min_ele(0) {
     }
     
     void push(int x) {
-        
+        stk.push_back(x);
+        if (min_ele.empty() || x < min_ele.back().second)
+            min_ele.push_back(make_pair(stk.size() - 1, x));
     }
     
     void pop() {
-        
+        stk.pop_back();
+        if (stk.size() <= min_ele.back().first)
+            min_ele.pop_back();
     }
     
     int top() {
-        
+        return stk.back();
     }
     
     int getMin() {
-        
+        return min_ele.back().second;
     }
-}
+};
 
 /**
  * Your MinStack object will be instantiated and called as such:
