c5575d83cf153872814bb5fcd56715bd460a0227
Xuenan Guo <boy2000_007man@163.com>
2016-05-11 15:21:20 +0800

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.cpp
+++ 204-Count-Primes/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n < 3)
-            return 0;
-        vector<int> primes(1, 2);
-        for (int k = 3; k < n; k += 2) {
-            bool p = true;
-            for (int i = 0; i < primes.size() && p && primes[i] * primes[i] <= k; i++)
-                p = k % primes[i];
-            if (p)
-                primes.push_back(k);
-        }
-        return primes.size();
+        int cnt = 2 < n ? 1 : 0;
+        vector<bool> bs(n, true);
+        for (int i = 3, t; (t = i * i) < n; i += 2)
+            if (bs[i])
+                do {
+                    bs[t] = false;
+                } while ((t += i) < n);
+        for (int i = 3; i < n; i += 2)
+            cnt += bs[i];
+        return cnt;
     }
 };
