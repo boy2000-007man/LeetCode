70933954d8e8baa153b4a1dfe7ae2195d6571ebd
Xuenan Guo <boy2000_007man@163.com>
2016-05-13 19:49:21 +0800

    [222. Count Complete Tree Nodes][Accepted]committed by LeetCode Extension

--- 222-Count-Complete-Tree-Nodes/solution.cpp
+++ 222-Count-Complete-Tree-Nodes/solution.cpp
@@ -9,16 +9,16 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-        int l = 0,
-            r = 0;
-        for (TreeNode *p = root; p; p = p->left)
-            l++;
-        for (TreeNode *p = root; p; p = p->right)
-            r++;
+    int countNodes(TreeNode* root, int l = 0, int r = 0) {
+        if (!l)
+            for (TreeNode *p = root; p; p = p->left)
+                l++;
+        if (!r)
+            for (TreeNode *p = root; p; p = p->right)
+                r++;
         if (l == r)
             return (1 << l) - 1;
         else
-            return countNodes(root->left) + 1 + countNodes(root->right);
+            return countNodes(root->left, l - 1, 0) + 1 + countNodes(root->right, 0, r - 1);
     }
 };
