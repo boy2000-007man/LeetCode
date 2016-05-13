f400f0da5f578e0408a6fd39ffe81171ac7acc78
Xuenan Guo <boy2000_007man@163.com>
2016-05-13 15:54:35 +0800

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.cpp
+++ 220-Contains-Duplicate-III/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
-    bool search(vector<pair<int, int>> &v, int l, int num, int k, int t) {
+    bool search(vector<pair<int, int>> &v, int l, int num, int k, unsigned t) {
         for (int i = v.size() - 1; 0 <= i && l - v[i].first <= k; i--)
-            if (abs((double)num - v[i].second) <= t)
+            if ((num < v[i].second ? v[i].second - num : num - v[i].second) <= t)
                 return true;
         v.push_back(make_pair(l, num));
         return false;
@@ -13,7 +13,9 @@ public:
             for (unsigned i = 0, bin = (t << 1) + 1; i < nums.size(); i++) {
                 int n = nums[i] / bin,
                     m = nums[i] % bin;
-                if (search(um[n], i, nums[i], k, t) || (m != t && search(m < t ? um[n - 1] : um[n + 1], i, nums[i], k, t)))
+                if (search(um[n], i, nums[i], k, t)
+                || (m < t && search(um[n - 1], i, nums[i], k, t))
+                || (t < m && search(um[n + 1], i, nums[i], k, t)))
                     return true;
             }
         }
