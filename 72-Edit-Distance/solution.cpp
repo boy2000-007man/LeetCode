0914e8deea27487698da1bf55cfaf9650f8fd4ce
Xuenan Guo <boy2000_007man@163.com>
2016-05-15 18:33:20 +0800

    [72. Edit Distance][Accepted]committed by LeetCode Extension

--- 72-Edit-Distance/solution.cpp
+++ 72-Edit-Distance/solution.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> result(word1.length() + 1, vector<int>(word2.length() + 1, INT_MAX));
-        for (int i = 0; i < result.size(); i++)
-            for (int j = 0; j < result[i].size(); j++)
+        int len1 = word1.length(),
+            len2 = word2.length();
+        if (len1 < len2) {
+            word1.swap(word2);
+            swap(len1, len2);
+        }
+        vector<vector<int>> result(len1 + 1, vector<int>(len2 + 1, len1));
+        for (int i = 0; i <= len1; i++)
+            for (int j = max(0, i + (len2 >> 1) - len1); j <= min(len2, i + (len2 >> 1)); j++)
                 if (!i && !j)
                     result[i][j] = 0;
                 else {
