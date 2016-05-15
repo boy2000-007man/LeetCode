0847ce770fa237f8f9f52d137404c0f1500bcd4d
Xuenan Guo <boy2000_007man@163.com>
2016-05-15 21:43:27 +0800

    [18. 4Sum][Accepted]committed by LeetCode Extension

--- 18-4Sum/solution.cpp
+++ 18-4Sum/solution.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> result;
-        vector<pair<int, int>> sum2;
         sort(nums.begin(), nums.end());
-        int len = 0;
+        vector<int> loc;
         for (int i = 0, cnt; i < nums.size(); i++)
-            if (!i || nums[i - 1] != nums[i])
-                nums[len++] = nums[i], cnt = 1;
-            else if (++cnt <= 4)
-                nums[len++] = nums[i];
-        nums.resize(len);
+            if (!i || nums[i - 1] != nums[i]) {
+                nums[loc.size()] = nums[i];
+                loc.push_back(loc.size());
+                cnt = 1;
+            } else if (++cnt <= 4) {
+                nums[loc.size()] = nums[i];
+                loc.push_back(loc.back());
+            }
+        nums.resize(loc.size());
+        vector<pair<int, int>> sum2;
         for (int i = 0; i < nums.size(); i++)
             for (int j = i + 1; j < nums.size(); j++)
                 sum2.push_back(make_pair(nums[i] + nums[j], i * nums.size() + j));
         sort(sum2.begin(), sum2.end());
+        unordered_map<int, int> um;
         for (int l = 0, r = sum2.size() - 1; l < r; l++) {
             while (l < r && target < sum2[l].first + sum2[r].first)
                 r--;
@@ -22,14 +27,17 @@ public:
                 vector<int> abcd = {static_cast<int>(sum2[l].second / nums.size()), static_cast<int>(sum2[l].second % nums.size()), static_cast<int>(sum2[i].second / nums.size()), static_cast<int>(sum2[i].second % nums.size())};
                 sort(abcd.begin(), abcd.end());
                 if (unique(abcd.begin(), abcd.end()) == abcd.end()) {
-                    for (int &a: abcd)
-                        a = nums[a];
-                    result.push_back(abcd);
+                    int order = 0;
+                    for (int a: abcd)
+                        order = order * nums.size() + loc[a];
+                    if (!um[order]++) {
+                        for (int &a: abcd)
+                            a = nums[a];
+                        result.push_back(abcd);
+                    }
                 }
             }
         }
-        sort(result.begin(), result.end());
-        result.erase(unique(result.begin(), result.end()), result.end());
         return result;
     }
 };
