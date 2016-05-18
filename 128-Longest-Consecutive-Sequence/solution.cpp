bc945c6dfa5ceef26065c68c9ee8e2d3a1385b00
Xuenan Guo <boy2000_007man@163.com>
2016-05-18 15:05:59 +0800

    [128. Longest Consecutive Sequence][Accepted]committed by LeetCode Extension

--- 128-Longest-Consecutive-Sequence/solution.cpp
+++ 128-Longest-Consecutive-Sequence/solution.cpp
@@ -14,19 +14,20 @@ public:
         int lcs = 0;
         vector<int> space(nums.size() << 1, nums[0]);
         for (int i = 0; i < ums.size(); i++)
-            for (auto &p: ums[i]) {
-                for (int num: p.second)
-                    space[mod_(num + i * nums.size(), nums.size() << 1)] = num;
-                for (int num: p.second) {
-                    int loc = mod_(num + i * nums.size(), nums.size() << 1),
-                        len = 1;
-                    for (int j = 1; loc + j < space.size() && space[loc + j] - num == j; j++, len++)
-                        space[loc + j] = num;
-                    for (int j = 1; 0 <= loc - j && num - space[loc - j] == j; j++, len++)
-                        space[loc - j] = num;
-                    lcs = max(lcs, len);
+            for (auto &p: ums[i])
+                if (lcs < p.second.size()) {
+                    for (int num: p.second)
+                        space[mod_(num + i * nums.size(), nums.size() << 1)] = num;
+                    for (int num: p.second) {
+                        int loc = mod_(num + i * nums.size(), nums.size() << 1),
+                            len = 1;
+                        for (int j = 1; loc + j < space.size() && space[loc + j] - num == j; j++, len++)
+                            space[loc + j] = num;
+                        for (int j = 1; 0 <= loc - j && num - space[loc - j] == j; j++, len++)
+                            space[loc - j] = num;
+                        lcs = max(lcs, len);
+                    }
                 }
-            }
         return lcs;
     }
 };
