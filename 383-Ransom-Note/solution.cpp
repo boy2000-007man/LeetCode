daab13409f3bf7aecd16363a20206ed60b8f1bf5
Xuenan Guo <boy2000_007man@163.com>
2016-09-15 01:20:19 -0700

    [383. Ransom Note][Accepted]committed by LeetCode Extension

---
+++ 383-Ransom-Note/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> cnt(26, 0);
+        for (auto c: magazine)
+            cnt[c - 'a']++;
+        for (auto c: ransomNote)
+            if (--cnt[c - 'a'] < 0)
+                return false;
+        return true;
+    }
+};
